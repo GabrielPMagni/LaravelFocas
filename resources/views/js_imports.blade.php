6cd068aad285686fbca143dd448f3a866c364cb3
Gabriel Peres Magni <gpmagni@protonmail.com>
2021-11-20 11:08:41 -0300

    commit 1

---
+++ resources/views/js_imports.blade.php
@@ -0,0 +1,3 @@
+@if (in_array('charts', $requirements))
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+@endif
---
+++ resources/views/main.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Home | Focas</title>
+        <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+    </head>
+    <body>
+        
+        @include('js_imports', ['requirements' => ['charts']]);
+    </body>
+</html>
